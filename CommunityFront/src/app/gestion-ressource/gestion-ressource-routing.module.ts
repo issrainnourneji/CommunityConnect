fd972c777c1faccc05fadc10831281359b7de6ae
chediamdouni <62970323+chediamdouni@users.noreply.github.com>
2023-10-26 02:43:04 +0200

    Implemented resource editing feature

--- CommunityFront/src/app/gestion-ressource/gestion-ressource-routing.module.ts
+++ CommunityFront/src/app/gestion-ressource/gestion-ressource-routing.module.ts
@@ -3,11 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 import { ListRessourceComponent } from './list-ressource/list-ressource.component';
 import { AddRessourceComponent } from './add-ressource/add-ressource.component';
 import { UpdateRessourceComponent } from './update-ressource/update-ressource.component';
+import { DeleteRessourceComponent } from './delete-ressource/delete-ressource.component';
 
 const routes: Routes = [
     {path : '' , component : ListRessourceComponent},
     {path :'add' , component : AddRessourceComponent},
-    {path :'update/:idR' , component : UpdateRessourceComponent}
+    {path :'update/:idR' , component : UpdateRessourceComponent},
+    {path :'delete/:idR' , component : DeleteRessourceComponent}
   ];
   
   @NgModule({
--- CommunityFront/src/app/gestion-ressource/update-ressource/update-ressource.component.ts
+++ CommunityFront/src/app/gestion-ressource/update-ressource/update-ressource.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Ressource } from 'src/app/Models/ressource';
+import { RessourceServiceService } from 'src/app/Services/ressource-service.service';
 
 @Component({
   selector: 'app-update-ressource',
@@ -7,9 +11,51 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UpdateRessourceComponent implements OnInit {
 
-  constructor() { }
+  myId=0;
+  myRessource!:Ressource;
+  reactiveForm=this.fb.group(
+    {
+      idR:[''],
+      name:[''],
+      type:[''],
+      location:[''],
+      owner:['']
+    }
+  );
+  constructor(private AR:ActivatedRoute,private fb:FormBuilder, private R:Router, private ressourceS:RessourceServiceService ) { }
 
   ngOnInit(): void {
+    this.ressourceS.getRessourceById(this.AR.snapshot.params["idR"]).subscribe(data=>{
+      this.myRessource=data;
+
+
+      this.reactiveForm.patchValue({idR: this.myRessource.idR ,name:this.myRessource.name,type:this.myRessource.type,location:this.myRessource.location, owner:this.myRessource.owner});
+      console.log(this.myRessource.name)
+    });
+}
+  update(){
+    this.ressourceS.UpdateRessource(this.reactiveForm.value).subscribe(data=>{
+
+      this.R.navigate(['Ressource']);
+      }
+      )
+  }
+  get idR(){
+    return this.reactiveForm.get('idR')
+  }
+  get name(){
+    return this.reactiveForm.get('name');
+
+  }
+  get type(){
+    return this.reactiveForm.get('type')
+  }
+  get location(){
+    return this.reactiveForm.get('location');
+
+  }
+  get owner(){
+    return this.reactiveForm.get('owner')
   }
 
 }
